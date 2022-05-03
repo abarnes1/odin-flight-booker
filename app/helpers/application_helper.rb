212e35bb89bd9d3094be7d4ad94c7a43c98bfe7f
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-05-03 13:07:29 -0500

    Add footer quote

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def footer_quote
+    [
+      'We let loose the goose!',
+      'We get loosey goosey with aircraft maintenance!',
+      'When Spirit is still too nice!'
+    ].sample
+  end
 end
