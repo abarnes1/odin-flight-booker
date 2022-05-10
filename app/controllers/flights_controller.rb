795813857e5f280364a95d9bb01f5be58fa2fd27
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-05-09 22:52:28 -0500

    Update views for time zone changes

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,18 +1,39 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.order(:code).map { |airport| ["#{airport.code} (#{airport.name})", airport.id] }
+    return if flight_search_params.empty?
 
-    @flight_search_results = search_flights(params)
+    @search_results = search_flights(flight_search_params)
   end
 
   private
 
+  def flight_search_params
+    params.permit(:departure_airport_id, :arrival_airport_id, :departure_date, :passenger_count)
+  end
+
   def search_flights(params)
     flights = Flight.all
-    flights = flights.where('start_datetime >= ?', Time.now) if params[:start_date].present?
+
+    if params[:departure_date].present?
+      search_start = local_airport_time_to_utc
+      search_range = search_start..(search_start + 1.days)
+
+      # This would cause missing search results for late but before midnight flights
+      # flights = flights.where(start_datetime: Time.find_zone('UTC').parse(params[:departure_date]).all_day)
+
+      flights = flights.where(start_datetime: search_range)
+    end
+
     flights = flights.where(departure_airport_id: params[:departure_airport_id]) if params[:departure_airport_id].present?
     flights = flights.where(arrival_airport_id: params[:arrival_airport_id]) if params[:arrival_airport_id].present?
 
     flights
+  end
+
+  def local_airport_time_to_utc
+    airport = Airport.find_by_id(params[:departure_airport_id])
+    airport_local_time = airport.local_time_zone.parse(params[:departure_date])
+    airport_local_time.in_time_zone('UTC')
   end
 end
