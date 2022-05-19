fddf1f27dc09acb39fabeee64934114972ee4bb9
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-05-18 21:53:19 -0500

    Add flight factory

--- spec/factories/flights.rb
+++ spec/factories/flights.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :flight do
-    
+    transient do
+      departing { nil }
+      arriving { nil }
+      at { departing.local_time + 1.hours }
+    end
+
+    departure_airport_id { departing.id }
+    arrival_airport_id { arriving.id }
+    departure_datetime { at }
+
+    duration { FlightDurationCalculator.flight_minutes(departing, arriving) }
   end
 end
