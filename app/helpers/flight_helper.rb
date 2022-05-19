2241bb29542ba3dd8bdbb4e214f6df2f5dd40e72
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-05-18 22:06:43 -0500

    Add flight summary helper

--- app/helpers/flight_helper.rb
+++ app/helpers/flight_helper.rb
@@ -2,4 +2,10 @@ module FlightHelper
   def display_datetime(datetime)
     datetime.strftime("%m/%d/%Y %I:%M %p")
   end
+
+  def summary(flight)
+    "#{flight.departure_airport.code} #{display_datetime(flight.local_departure_time)} -> " +
+    "#{flight.arrival_airport.code} #{display_datetime(flight.local_arrival_time)}" +
+    "(#{flight.duration} minutes)."
+  end
 end
