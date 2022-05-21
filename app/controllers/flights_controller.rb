6c35559e31bba00a9afcca4698cdd11e49c00a11
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-05-21 18:34:15 -0500

    Fix search results displaying on first visit

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.order(:code).map { |airport| ["#{airport.code} (#{airport.name})", airport.id] }
-    return if flight_search_params.empty?
+    return unless search?
 
     @search_results = search_flights(flight_search_params)
   end
@@ -9,19 +9,17 @@ class FlightsController < ApplicationController
   private
 
   def flight_search_params
-    params.permit(:departure_airport_id, :arrival_airport_id, :departure_date, :passenger_count)
+    params.permit(:departure_airport_id, :arrival_airport_id, :departure_date, :passenger_count, :commit)
+  end
+
+  def search?
+    %i[departure_airport_id arrival_airport_id].all? { |key| params.include? key }
   end
 
   def search_flights(params)
     flights = Flight.all
 
-    if params[:departure_date].present?
-      search_start = local_airport_time_to_utc
-      search_range = search_start..(search_start + 1.days)
-
-      flights = flights.where(departure_datetime: search_range)
-    end
-
+    flights = flights.where(departure_datetime: day_search_range)
     flights = flights.where(departure_airport_id: params[:departure_airport_id]) if params[:departure_airport_id].present?
     flights = flights.where(arrival_airport_id: params[:arrival_airport_id]) if params[:arrival_airport_id].present?
 
@@ -32,5 +30,10 @@ class FlightsController < ApplicationController
     airport = Airport.find_by_id(params[:departure_airport_id])
     airport_local_time = airport.local_time_zone.parse(params[:departure_date])
     airport_local_time.in_time_zone('UTC')
+  end
+
+  def day_search_range
+    search_start = local_airport_time_to_utc
+    search_start..(search_start + 1.days)
   end
 end
