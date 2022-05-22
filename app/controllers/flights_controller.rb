d815273af4cc8b21316502905f99bd9f1bb09555
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-05-21 19:05:15 -0500

    Prevent searching flights to and from the same airport

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.order(:code).map { |airport| ["#{airport.code} (#{airport.name})", airport.id] }
-    return unless search?
+    return unless valid_search?
 
     @search_results = search_flights(flight_search_params)
   end
@@ -12,16 +12,23 @@ class FlightsController < ApplicationController
     params.permit(:departure_airport_id, :arrival_airport_id, :departure_date, :passenger_count, :commit)
   end
 
-  def search?
-    %i[departure_airport_id arrival_airport_id].all? { |key| params.include? key }
+  def valid_search?
+    return false unless %i[departure_airport_id arrival_airport_id].all? { |key| params.include? key }
+
+    if params[:departure_airport_id] == params[:arrival_airport_id]
+      flash.now[:alert] = "From and To can't be the same."
+      return false
+    end
+
+    true
   end
 
   def search_flights(params)
     flights = Flight.all
 
     flights = flights.where(departure_datetime: day_search_range)
-    flights = flights.where(departure_airport_id: params[:departure_airport_id]) if params[:departure_airport_id].present?
-    flights = flights.where(arrival_airport_id: params[:arrival_airport_id]) if params[:arrival_airport_id].present?
+    flights = flights.where(departure_airport_id: params[:departure_airport_id])
+    flights = flights.where(arrival_airport_id: params[:arrival_airport_id])
 
     flights
   end
