f0470ec2b30a1318ed9af47c0f89f1747d57bff3
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-05-28 19:59:00 -0500

    Add booking creation
    - Bookings can be saved with very rough, rudimentary values
    - First commit where this works but needs further cleanup

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,39 @@
 class BookingsController < ApplicationController
   def new
+    @flight = Flight.find(booking_params[:flight_id])
+    @booking = @flight.bookings.build(booking_params)
+
+    # passengers = params[:passenger_count].present? ? params[:passenger_count].to_i : 1
+    passengers = params[:booking][:passenger_count].to_i 
+    passengers.times { @booking.passengers.build }
   end
 
   def show
+
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    generate_confirmation_code
+
+    debugger
+
+
+    @booking.save
+
+    redirect_to :root
+  end
+
+  private
+  
+  def booking_params
+    params.require(:booking).permit(:flight_id, :name, :email, passengers_attributes: [:name, :email])
+  end
+
+  def generate_confirmation_code
+    temp_code = String.new
+    10.times { temp_code << rand(65..90).chr }
+
+    @booking.confirmation_code = temp_code
   end
 end
