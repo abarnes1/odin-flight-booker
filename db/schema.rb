697f92fd4a0d638cbdc3e99589c54faa164c3895
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-06-04 18:53:56 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_230511) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_04_232445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,7 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_25_230511) do
     t.string "confirmation_code", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["confirmation_code"], name: "index_bookings_on_confirmation_code"
+    t.index ["confirmation_code"], name: "index_bookings_on_confirmation_code", unique: true
     t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
