d41ba4b12bbd6e88dfb64621684cfbd419da9350
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-06-05 11:28:01 -0500

    Create and use flight summary partial and datetime display helpers

--- app/helpers/flight_helper.rb
+++ app/helpers/flight_helper.rb
@@ -8,4 +8,16 @@ module FlightHelper
     "#{flight.arrival_airport.code} #{display_datetime(flight.local_arrival_time)}" \
     " (#{flight.duration} minutes)."
   end
+
+  def display_time(time)
+    time.strftime("%l:%M %p")
+  end
+
+  def display_duration(minutes)
+    "#{minutes / 60} h #{minutes % 60} mins"
+  end
+
+  def display_departure_date(time)
+    time.strftime("%a %b %-d, %Y")
+  end
 end
