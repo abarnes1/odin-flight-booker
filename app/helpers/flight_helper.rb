1886c14693c789d445f02ab64314d9d34507baff
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-06-05 12:44:39 -0500

    Code cleanup

--- app/helpers/flight_helper.rb
+++ app/helpers/flight_helper.rb
@@ -1,16 +1,6 @@
 module FlightHelper
-  def display_datetime(datetime)
-    datetime.strftime('%m/%d/%Y %I:%M %p')
-  end
-
-  def flight_summary(flight)
-    "#{flight.departure_airport.code} #{display_datetime(flight.local_departure_time)} -> " \
-    "#{flight.arrival_airport.code} #{display_datetime(flight.local_arrival_time)}" \
-    " (#{flight.duration} minutes)."
-  end
-
   def display_time(time)
-    time.strftime("%l:%M %p")
+    time.strftime('%l:%M %p')
   end
 
   def display_duration(minutes)
@@ -18,6 +8,6 @@ module FlightHelper
   end
 
   def display_departure_date(time)
-    time.strftime("%a %b %-d, %Y")
+    time.strftime('%a %b %-d, %Y')
   end
 end
