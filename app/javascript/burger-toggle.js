029a21e7203f7d1216b8d6a02b993493f873bf55
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-06-10 16:52:12 -0500

    Fix burger menu not always loading
    - I believe this is turbo related and have forced turbo to ensure
      a full reload between page visits.  Not ideal, but the only thing
      I've been able to get working.

--- app/javascript/burger-toggle.js
+++ app/javascript/burger-toggle.js
@@ -4,6 +4,7 @@ const navbarMenu = document.querySelector('#nav-links')
 function registerBurgerToggle() {
   burgerIcon.addEventListener('click', (event) => {
     navbarMenu.classList.toggle('is-active');
+    burgerIcon.classList.toggle('is-active');
   });  
 };
 
